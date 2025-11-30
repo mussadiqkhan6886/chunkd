34c0735928bae8390a69dd8cd793e4c714fd8169
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-12-01 01:03:10 +0500

    menu completed

--- app/(main)/menu/[slug]/page.tsx
+++ app/(main)/menu/[slug]/page.tsx
@@ -1,11 +1,18 @@
 import SingleProduct from "@/components/mainComp/SingleProduct";
+import { connectDB } from "@/lib/config/databse";
+import CookieSchema from "@/lib/models/CookieSchema";
 import React from "react";
 
 const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const { slug } = await params;
+  await connectDB()
+  
+  const res = await CookieSchema.find({slug: slug}).lean()
+  
+  const data = JSON.parse(JSON.stringify(res))
 
   return (
-    <SingleProduct slug={slug} />
+    <SingleProduct data={data[0]} />
   )
 };
 
