d86f36a1a7bdb43932a28650d9282e1df53ec04d
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-12-14 21:25:14 +0500

    fixed newsletter

--- components/mainComp/SubscribeAb.tsx
+++ components/mainComp/SubscribeAb.tsx
@@ -1,12 +1,25 @@
 'use client';
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Subscribe from './Subscribe'
-import { FiX, FiXCircle } from 'react-icons/fi';
-import { FaXRay } from 'react-icons/fa';
+import {  FiXCircle } from 'react-icons/fi';
 
 const SubscribeAb = () => {
     const [cross, setCross] = useState(false)
+    const [show, setShow] = useState(false)
+
+    useEffect(() => {
+      const hasSeen = sessionStorage.getItem("seenNewsLetter")
+
+      if(!hasSeen){
+        setShow(true)
+        sessionStorage.setItem("seenNewsLetter", "true")
+      }
+
+    }, [])
+
+    if(!show) return null
+
   return (
     <div className={`${cross ? "hidden" : "flex flex-col items-center"} comeUpBig fixed z-50 md:scale-75 left-0 right-0 w-full`}>
         <FiXCircle className='text-center text-[30px] md:text-[40px] cursor-pointer' onClick={() => setCross(true)} />
