d8c392a628530ae8f8c0d7aee456e5706f2f4bef
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-12-18 13:30:38 +0500

    done

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -8,6 +8,7 @@ const Hero = () => {
  
   return (
     <section className='width relative h-screen md:h-[80vh]' >
+      <div className='absolute w-full h-full bg-soft/10 inset-0' />
       <video src={"/hero.mp4"} className='w-full h-full object-cover object-center' loop autoPlay muted playsInline /> 
     <NoticeBanner />
       <div className="absolute w-full lg:w-[50%] left-1/2 top-1/2 -translate-x-1/2 translate-y-10 md:translate-y-20 flex items-center flex-col gap-4">
