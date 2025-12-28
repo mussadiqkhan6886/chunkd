e5245b73a4be665684e0630cd1787a6f5c12b765
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-12-29 02:56:40 +0500

    completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { mainFont } from "@/lib/font/font";
 import { DropProvider } from "@/lib/context/contextAPI";
-import dynamic from "next/dynamic";
-const MetaPixel = dynamic(() => import("@/components/adminComp/MetaPixel"))
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Chunk'd",
@@ -12,19 +11,46 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${mainFont.className} antialiased`}
-      >
-        <DropProvider>
-        <MetaPixel />
-        {children}
-        </DropProvider>
+      <head>
+        {/* Meta Pixel */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1402453134877505');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
 
+        {/* NoScript Fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1402453134877505&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+      </head>
+
+      <body className={`${mainFont.className} antialiased`}>
+        <DropProvider>{children}</DropProvider>
       </body>
     </html>
   );
